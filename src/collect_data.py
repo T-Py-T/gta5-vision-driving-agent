6f67202878b3cf068bcda35bbe85dee4a8eb909d
taylor <tnt850910@aol.com>
2026-08-21 01:16:33 -0400

    feat: make vision agent show ready

--- src/collect_data.py
+++ src/collect_data.py
@@ -3,17 +3,8 @@ from grabscreen import grab_screen
 import cv2
 import time
 from getkeys import key_check
+from policy import keys_to_output
 import os
-
-w = [1,0,0,0,0,0,0,0,0]
-s = [0,1,0,0,0,0,0,0,0]
-a = [0,0,1,0,0,0,0,0,0]
-d = [0,0,0,1,0,0,0,0,0]
-wa = [0,0,0,0,1,0,0,0,0]
-wd = [0,0,0,0,0,1,0,0,0]
-sa = [0,0,0,0,0,0,1,0,0]
-sd = [0,0,0,0,0,0,0,1,0]
-nk = [0,0,0,0,0,0,0,0,1]
 
 starting_value = 1
 
@@ -27,35 +18,6 @@ while True:
         print('File does not exist, starting fresh!',starting_value)
         
         break
-
-
-def keys_to_output(keys):
-    '''
-    Convert keys to a ...multi-hot... array
-     0  1  2  3  4   5   6   7    8
-    [W, S, A, D, WA, WD, SA, SD, NOKEY] boolean values.
-    '''
-    output = [0,0,0,0,0,0,0,0,0]
-
-    if 'W' in keys and 'A' in keys:
-        output = wa
-    elif 'W' in keys and 'D' in keys:
-        output = wd
-    elif 'S' in keys and 'A' in keys:
-        output = sa
-    elif 'S' in keys and 'D' in keys:
-        output = sd
-    elif 'W' in keys:
-        output = w
-    elif 'S' in keys:
-        output = s
-    elif 'A' in keys:
-        output = a
-    elif 'D' in keys:
-        output = d
-    else:
-        output = nk
-    return output
 
 
 def main(file_name, starting_value):
